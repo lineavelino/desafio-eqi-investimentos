53b027b3a733467126177719313ac6a1a40ce9de
Aline Avelino <aline.savelino@gmail.com>
2022-02-09 15:12:11 -0300

    Adicionar componentes na tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { Results } from "./components/Results";
+import { Simulator } from "./components/Simulator";
+import styles from "./App.module.scss";
+
 export function App() {
   return (
     <>
       <header>
-    <h1>Simulador de Investimentos</h1>
+        <h1>Simulador de Investimentos</h1>
       </header>
+      <main className={styles.container}>
+        <Simulator />
+        <Results />
+      </main>
     </>
   )
 }
